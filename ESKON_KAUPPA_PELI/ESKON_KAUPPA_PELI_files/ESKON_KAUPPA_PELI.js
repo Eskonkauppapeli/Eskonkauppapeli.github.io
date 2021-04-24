05f12a2f88acba6566d36a43470e3fe0526bc218
Brunkki <bruno@mladens-iMac.lan>
2021-04-24 16:51:40 +0300

    all stuff added

---
+++ ESKON_KAUPPA_PELI/ESKON_KAUPPA_PELI_files/ESKON_KAUPPA_PELI.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20210424
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ESKON_KAUPPA_PELI_files/ESKON_KAUPPA_PELIMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id6');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
